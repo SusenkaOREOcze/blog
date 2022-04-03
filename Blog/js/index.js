4012dc2364596d77409ff1f9220a3d681f59aaca
SusenkaOREOcz <70967583+SusenkaOREOcze@users.noreply.github.com>
2022-04-03 08:50:42 +0200

    Add files via upload

---
+++ Blog/js/index.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var x = document.getElementById("menu_container");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
---
+++ Blog/js/kontakt.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+    var date = new Date(date.getFullYear(), date.getMonth(), date.getDay());
+    
+    console.log(date);
+}, 1000)
