50a98a6d71a3432f93a0c0ef8e1ba7a2fb6f0f61
SusenkaOREOcz <70967583+SusenkaOREOcze@users.noreply.github.com>
2022-04-05 17:05:20 +0200

    Delete Blog directory

--- Blog/js/index.js
+++
@@ -1,8 +0,0 @@
-function showMenu() {
-    var x = document.getElementById("menu_container");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
--- Blog/js/kontakt.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function() {
-    var date = new Date(date.getFullYear(), date.getMonth(), date.getDay());
-    
-    console.log(date);
-}, 1000)
